b79b7fe4b6ed6a8ae568233bb93c1f9ebed4e6dc
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-09-23 21:20:23 +0530

    Messaages app

---
+++ projects/messages/src/messages/messages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { MessagesRepository } from './messages.repository';
+
+@Injectable()
+export class MessagesService {
+  constructor(private messagesRepo: MessagesRepository) { }
+
+  async findOne(id: string) {
+    return this.messagesRepo.findOne(id)
+  }
+
+  async findAll() {
+    return this.messagesRepo.findAll()
+  }
+
+  async create(message: string) {
+    return this.messagesRepo.create(message)
+  }
+}
